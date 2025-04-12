259b4164f77c3a7846a976d01366a8e456da2b04
Kush Tiwari <tiwarikush9328@gmail.com>
2025-04-13 00:59:27 +0530

    Add test: negative numbers should throw an exception

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -27,5 +27,12 @@ class StringCalculatorTest < Minitest::Test
     assert_equal 3, calc.add("//;\n1;2")
   end
   
+  def test_negative_numbers
+    calc = StringCalculator.new
+    assert_raises(RuntimeError, "negative numbers not allowed -1") do
+      calc.add("-1,5")
+    end
+  end
+  
   
 end
